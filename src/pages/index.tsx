139c2d56a382a31bc0ffab23e0169746bc592f79
LeviReg <leviarcanine22@gmail.com>
2021-12-16 07:39:41 +0000

    Image test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -46,6 +46,8 @@ export default function Page() {
         <section className={styles.explore}>
           <div className="wrap">
             <h2>Explore this Example Project</h2>
+            <img src='/images/city.jpg'></img>
+            <img src='/images/egypt.jpg'></img>
             <p>
             Anyone Without A Ship Should Secure A Weapon & Fire Wildly Into The Air.
             </p>
